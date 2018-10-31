0508ad6c23c684888d76ae1b97fd31e06ca720b6
Max Kaye <m@xk.io>
2018-10-31 20:53:17 +1100

    Fix up all the broken things

--- scripts/convertMarkdown.sh
+++ scripts/convertMarkdown.sh
@@ -3,18 +3,22 @@ set -e
 IFS=$'\n'
 mkdir -p _dist
 DATE=$(TZ=Australia/Sydney date +%Y-%m-%d--%H-%M)
-FORMATS=(docx pdf txt epub rst html odt rtf)
+FORMATS=(docx pdf plain epub rst html odt rtf)
 echo "$DATE" > ./theDate
 for f in $(find . | grep 'md$'); do
-    for FORMAT in $FORMATS; do
-        FNAME=$(basename -s "$f")
+    for FORMAT in ${FORMATS[@]}; do
+        FNAME=$(basename -s "md" "$f")
         DIRNAME=$(dirname "$f")
+        mkdir -p "_dist/$DIRNAME"
         EXT="$FORMAT"
         if [ "$FORMAT" == "pdf" ]; then
-            FORMAT="html5"
+            FORMAT="ms"
             EXT="pdf"
         fi
-        LAST_EDIT=$(TZ=Australia/Sydney date -d @$(git log --format=%at -- %f | head -1) +%Y-%m-%d--%H-%M)
+        if [ "$FORMAT" = "plain" ]; then
+            EXT="txt"
+        fi
+        LAST_EDIT=$(TZ=Australia/Sydney date -d @$(git log --format=%at -- "$f" | head -1) +%Y-%m-%d--%H-%M)
         OUTNAME="_dist/$DIRNAME/$FNAME.last-edited.$LAST_EDIT.$EXT"
         pandoc --from gfm --to $FORMAT $f -o $OUTNAME &
     done

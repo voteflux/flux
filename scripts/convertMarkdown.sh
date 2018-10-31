e534fd29d4e22416c6b2e9a38c9a9aaaa23ecdac
Max Kaye <m@xk.io>
2018-10-31 23:04:54 +1100

    add buildspec
    - remove rtf because it wasn't working

--- scripts/convertMarkdown.sh
+++ scripts/convertMarkdown.sh
@@ -3,11 +3,11 @@ set -e
 IFS=$'\n'
 mkdir -p _dist
 DATE=$(TZ=Australia/Sydney date +%Y-%m-%d--%H-%M)
-FORMATS=(docx pdf plain epub rst html odt rtf)
+FORMATS=(docx pdf plain epub rst html odt)
 echo "$DATE" > ./theDate
 for f in $(find . | grep 'md$'); do
     for FORMAT in ${FORMATS[@]}; do
-        FNAME=$(basename -s "md" "$f")
+        FNAME=$(basename -s ".md" "$f")
         DIRNAME=$(dirname "$f")
         mkdir -p "_dist/$DIRNAME"
         EXT="$FORMAT"
@@ -20,6 +20,7 @@ for f in $(find . | grep 'md$'); do
         fi
         LAST_EDIT=$(TZ=Australia/Sydney date -d @$(git log --format=%at -- "$f" | head -1) +%Y-%m-%d--%H-%M)
         OUTNAME="_dist/$DIRNAME/$FNAME.last-edited.$LAST_EDIT.$EXT"
+        echo "$FNAME" "<- FNAME"
         pandoc --from gfm --to $FORMAT $f -o $OUTNAME &
     done
 done
